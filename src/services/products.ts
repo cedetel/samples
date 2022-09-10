f7b53936d24cf78f20b402cc6fc8e4766463b924
cedetel <camanacob.etel@gmail.com>
2022-09-11 00:27:35 +0800

    test

--- src/services/products.ts
+++ src/services/products.ts
@@ -6,13 +6,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 export const getProducts = async () => {
   let response: IGetProductsResponse;
 
-  if (isProduction) {
-    response = await axios.get(
-      'https://react-shopping-cart-67954.firebaseio.com/products.json'
-    );
-  } else {
+  // if (isProduction) {
+  //   response = await axios.get(
+  //     'https://react-shopping-cart-67954.firebaseio.com/products.json'
+  //   );
+  // } else {
     response = require('static/json/products.json');
-  }
+  // }
 
   const { products } = response.data || [];
 
